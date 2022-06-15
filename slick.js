0ff716c03ece089fe1826a605aee236c3a583e44
Electro1212 <akashdebnath379@gmail.com>
2022-06-15 14:58:30 +0530

    bur marani Akash

---
+++ slick.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true, 
+        },
+        600: {
+          items: 1,
+          nav: false,
+        },
+        1000: {
+          items: 1,
+          nav: true,
+          loop: false,
+        }
+      }
+    })
+  })
